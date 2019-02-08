bee3eb8aa2c78319606066245495fea6bdc79490
AnsurM <ansurmehdi@gmail.com>
2019-02-08 21:53:23 +0500

    Updates

---
+++ precache-manifest.b3d5bbda599c41377fa0e94891a3f6ce.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "1446d6ec194445c3572d",
+    "url": "/robofriends/static/js/runtime~main.1446d6ec.js"
+  },
+  {
+    "revision": "1c16ff556e6b5db3cb96",
+    "url": "/robofriends/static/js/main.1c16ff55.chunk.js"
+  },
+  {
+    "revision": "ae54adf789a07448178d",
+    "url": "/robofriends/static/js/1.ae54adf7.chunk.js"
+  },
+  {
+    "revision": "1c16ff556e6b5db3cb96",
+    "url": "/robofriends/static/css/main.e2fcefa6.chunk.css"
+  },
+  {
+    "revision": "ae54adf789a07448178d",
+    "url": "/robofriends/static/css/1.b9d0d9d9.chunk.css"
+  },
+  {
+    "revision": "eb2d25b7049eb123d072f57618a4b3cf",
+    "url": "/robofriends/index.html"
+  }
+];
